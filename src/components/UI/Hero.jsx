c2f804d9d90936af06608d1dca870164077338d7
Saeedmaldosary <saeedmaldosary@gmail.com>
2023-06-27 11:48:18 +0300

    Setup Hero component

---
+++ src/components/UI/Hero.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import heroImg from "../../assets/images/hero.svg";
+import CountUp from "react-countup";
+
+const Hero = () => {
+  return;
+  <section className="pt-0" id="about">
+    <div className="container"></div>
+  </section>;
+};
+
+export default Hero;
