1d0858ec4581c0e201d2e1acfd15efa41c8656b4
Saeedmaldosary <saeedmaldosary@gmail.com>
2023-06-27 23:33:15 +0300

    Add image to the body and set the grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Hero from "./components/UI/Hero";
 
 function App() {
   return (
     <>
       <Header />
-      <main></main>
+      <Hero />
       <Footer />
     </>
   );
--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -1,12 +1,38 @@
 import React from "react";
-import heroImg from "../../assets/images/hero.svg";
-import CountUp from "react-countup";
+import { Container, useTheme } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import saeedImage from "../../assets/images/Saeed.png";
 
 const Hero = () => {
-  return;
-  <section className="pt-0" id="about">
-    <div className="container"></div>
-  </section>;
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item md={8} xs={12}>
+          <div className="bg-slate-500">Content 1</div>
+        </Grid>
+        <Grid item md={4} xs={12}>
+          <div
+            style={{
+              backgroundColor: theme.palette.secondary.main, // Access the secondary color from theme
+              borderRadius: "35%",
+              overflow: "hidden",
+            }}
+          >
+            <img
+              style={{
+                marginTop: "90px", // Adjust the value as needed
+              }}
+              src={saeedImage}
+              alt="Transparent Image"
+            />
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Hero;
