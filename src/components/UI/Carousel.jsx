4873a4cf03ff1fdcd59829a9e513baf0f99763f8
Saeedmaldosary <saeedmaldosary@gmail.com>
2023-07-02 20:14:52 +0300

    Change items number based on screen size

--- src/components/UI/Carousel.jsx
+++ src/components/UI/Carousel.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import MUICarousel from "react-material-ui-carousel";
 import Grid from "@mui/material/Grid";
 import Card from "@mui/material/Card";
-import { useTheme } from "@mui/material";
+import { useTheme, useMediaQuery } from "@mui/material";
 
 function Carousel({ skillsData }) {
   const theme = useTheme();
 
-  const itemsPerRow = 6;
+  const isMdScreen = useMediaQuery(theme.breakpoints.up("md"));
+  const itemsPerRow = isMdScreen ? 6 : 3;
   const groupedSkillsData = [];
   for (let i = 0; i < skillsData.length; i += itemsPerRow) {
     groupedSkillsData.push(skillsData.slice(i, i + itemsPerRow));
