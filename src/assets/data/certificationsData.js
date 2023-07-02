ccbdb229e4a14a54359e6073bda334873452031e
Saeedmaldosary <saeedmaldosary@gmail.com>
2023-07-02 20:04:48 +0300

    Add more data

--- src/assets/data/certificationsData.js
+++ src/assets/data/certificationsData.js
@@ -1,11 +1,39 @@
 import APIAcademyImage from "../../assets/images/certifications/APIAcademy.png";
 import ScrumORGImage from "../../assets/images/certifications/ScrumORG.png";
 import UdacityImage from "../../assets/images/certifications/Udacity.png";
+import SoftwareAGImage from "../../assets/images/certifications/SoftwareAG.png";
 
 const certificationsData = [
-  { id: 1, image: APIAcademyImage, name: "APIAcademyImage" },
-  { id: 2, image: ScrumORGImage, name: "ScrumORGImage" },
-  { id: 3, image: UdacityImage, name: "UdacityImage" },
+  {
+    id: 1,
+    image: APIAcademyImage,
+    IssuingORG: "API Academy",
+    name: "API Designer",
+  },
+  {
+    id: 2,
+    image: UdacityImage,
+    IssuingORG: "Udacity",
+    name: "Intermediate JavaScript Nanodegree Program",
+  },
+  {
+    id: 3,
+    image: ScrumORGImage,
+    IssuingORG: "Scrum.org",
+    name: "Professional Scrum Master ",
+  },
+  {
+    id: 4,
+    image: SoftwareAGImage,
+    IssuingORG: "Software AG",
+    name: "Certified webMethods Integration Associate",
+  },
+  {
+    id: 5,
+    image: UdacityImage,
+    IssuingORG: "Udacity",
+    name: "Front-End Web Development Nanodegree Program",
+  },
 ];
 
 export default certificationsData;
