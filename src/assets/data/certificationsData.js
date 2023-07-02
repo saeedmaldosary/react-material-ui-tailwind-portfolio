dc311e9b2660d2a0857739efc043bee7127eb55e
Saeedmaldosary <saeedmaldosary@gmail.com>
2023-07-02 17:16:49 +0300

    Add certifications data

---
+++ src/assets/data/certificationsData.js
@@ -0,0 +1,11 @@
+import APIAcademyImage from "../../assets/images/certifications/APIAcademy.png";
+import ScrumORGImage from "../../assets/images/certifications/ScrumORG.png";
+import UdacityImage from "../../assets/images/certifications/Udacity.png";
+
+const certificationsData = [
+  { id: 1, image: APIAcademyImage, name: "APIAcademyImage" },
+  { id: 2, image: ScrumORGImage, name: "ScrumORGImage" },
+  { id: 3, image: UdacityImage, name: "UdacityImage" },
+];
+
+export default certificationsData;
