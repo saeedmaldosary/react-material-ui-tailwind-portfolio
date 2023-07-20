808e6f84a5ac9b28b09cc59e5d0ff290856096f5
Saeedmaldosary <saeedmaldosary@gmail.com>
2023-07-20 18:37:39 +0300

    Enhance the theme

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const theme = createTheme({
   typography: {
     allVariants: {
       fontFamily: "Urbanist",
+      // Below the color of text (not include the color of text in the buttons and the text in send me message card)
       color: "#193256",
     },
   },
@@ -17,23 +18,37 @@ const theme = createTheme({
         root: {
           "&:hover": {
             backgroundColor: "#564899",
-            color: "#ffff",
+            color: "#ffffff",
           },
+        },
+      },
+    },
+    MuiCarousel: {
+      styleOverrides: {
+        dot: {
+          backgroundColor: "#8873ef",
+        },
+        dotActive: {
+          backgroundColor: "#564899",
         },
       },
     },
   },
   palette: {
-    action: {
-      disabledBackground: "#564899",
-      disabled: "#ffff",
-    },
     primary: {
+      // Below for Header background color
+      // certifcation color backgtound(orgs logo background)
+      //  button of send me message card
+      // send me message text
+      // Color of Text in preload logo
       main: "#ffffff",
+      // Below the color of text for button in send me message card
       contrastText: "#8873ef",
     },
     secondary: {
+      // Below for Logo Preloader card and cards and buttons
       main: "#8873ef",
+      // Below for menu in big screen About , Skills, Certification
       contrastText: "#564899",
     },
   },
